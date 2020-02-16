9585aa9a398d183c27fc19443fdadacd7016d53e
Greg Miller <hello@gregmiller.io>
2020-02-16 17:11:20 -0500

    Add image size for hero

--- wp-content/themes/ar-mods/functions.php
+++ wp-content/themes/ar-mods/functions.php
@@ -20,7 +20,9 @@
         add_image_size('large', 700, '', true); // Large Thumbnail
         add_image_size('medium', 250, '', true); // Medium Thumbnail
         add_image_size('small', 120, '', true); // Small Thumbnail
+
         add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+        add_image_size('slider_image', 920, 500, true); // Header custom size
 
         // Enables post and comment RSS feed links to head
         add_theme_support('automatic-feed-links');
