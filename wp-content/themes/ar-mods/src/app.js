f0c51c2a53b9ab316c44e026d96c3326526c23a5
Greg Miller <hello@gregmiller.io>
2020-02-16 08:36:26 -0500

    Limit inclusion of Bootstrap; Add custom template and class stylesheet

--- wp-content/themes/ar-mods/src/app.js
+++ wp-content/themes/ar-mods/src/app.js
@@ -1,16 +1,16 @@
 import $ from 'jquery';
-import 'popper.js';
-import 'bootstrap/js/dist/alert';
-import 'bootstrap/js/dist/button';
-import 'bootstrap/js/dist/carousel';
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/modal';
-import 'bootstrap/js/dist/popover';
-import 'bootstrap/js/dist/scrollspy';
-import 'bootstrap/js/dist/tab';
-import 'bootstrap/js/dist/tooltip';
-import 'bootstrap/js/dist/util';
+// import 'popper.js';
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/tooltip';
+// import 'bootstrap/js/dist/util';
 
 // Styles
 import "./sass/style.scss";
